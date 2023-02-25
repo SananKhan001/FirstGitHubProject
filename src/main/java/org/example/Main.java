f419295f976dd64a84f22e816b74f7c1676592f1
Sanan Khan <sanan3946@gmail.com>
2023-02-25 23:36:56 +0530

    git init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("First GitHub Upload !!!");
+        System.out.println("Now writing some extra code to check if it updating or not !!!");
     }
 }
